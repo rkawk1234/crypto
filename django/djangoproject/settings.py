6d1040515a7a6057b5c3baa3a4bd0f52cb6191f0
rkawk1234 <okus6977@gmail.com>
2022-08-22 01:10:52 -0400

    popular changed
    table created

--- django/djangoproject/settings.py
+++ django/djangoproject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-     'django.contrib.sessions',
+    'django.contrib.sessions',
+    'django.contrib.humanize',
     'base',
     
 
